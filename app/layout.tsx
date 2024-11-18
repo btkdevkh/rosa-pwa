3a226ac709718c5678a0187b95c6993d3bb94bf0
Bunthoeun Kong <bunthoeun.kong@greenshield.fr>
2024-11-18 10:50:57 +0100

    feat: :tada: Init project

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  weight: ["100", "300", "400", "500", "700", "900"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Rospot",
+  description:
+    "Collecter des données terrain, analyser les données puis bénéficier de prévisions de risques sur la rouille de la rose de mai",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  );
+}
