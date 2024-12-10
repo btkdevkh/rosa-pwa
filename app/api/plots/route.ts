edef27a54883f5e274f20183f0c96dc8e18e0d0c
Bunthoeun Kong <bunthoeun.kong@greenshield.fr>
2024-12-10 15:57:17 +0100

    fix: :bug: Fix api call to the server in progress...

--- app/api/plots/route.ts
+++ app/api/plots/route.ts
@@ -1,13 +1,1 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { db } from "@/app/lib/db";
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function GET(request: NextApiRequest, response: NextApiResponse) {
-  const res = await db.exploitations.findMany();
-  console.log("res :", res);
-
-  return new Response(JSON.stringify({ message: "" }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
-}
+export async function GET() {}
