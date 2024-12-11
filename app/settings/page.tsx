3e80ce9ed48a575d6b902e04e09bf52a0741f0ec
Bunthoeun Kong <bunthoeun.kong@greenshield.fr>
2024-12-12 00:32:42 +0100

    fix:🐛 Troubleshoot on fetching data issue with Service Worker...

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,6 +1,72 @@
-// Server Component : Not working on fetching data on production
+// -------------------------------------
+// Client Component (Work on production)
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { OptionType } from "../components/selects/SingleSelect";
+import Loading from "../components/Loading";
+import SettingPageClient from "../components/clients/settings/SettingPageClient";
+import { Exploitation } from "../models/interfaces/Exploitation";
+import { useSession } from "next-auth/react";
+
+const SettingPage = () => {
+  const { data: session, status } = useSession(); // Use useSession hook to access session data
+  console.log("session :", session);
+
+  const [userExploitations, setUserExploitations] = useState<
+    OptionType[] | null
+  >(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchExploitations = async () => {
+      if (!session?.user?.name) {
+        setLoading(false);
+        return;
+      }
+
+      try {
+        const response = await fetch(
+          `${
+            process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000"
+          }/api/exploitations?userUID=${session.user.name}`
+        );
+        console.log("response :", response);
+
+        if (response.ok) {
+          const exploitations: Exploitation[] = await response.json();
+          const exploitationOptions = exploitations.map(exploitation => ({
+            id: exploitation.id,
+            value: exploitation.nom,
+            label: exploitation.nom,
+          }));
+
+          setUserExploitations(exploitationOptions);
+        } else {
+          console.error("Failed to fetch exploitations: ", response.status);
+        }
+      } catch (error) {
+        console.error("Error fetching exploitations: ", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchExploitations();
+  }, [session]);
+
+  if (status === "loading" || loading) {
+    return <Loading />;
+  }
+
+  return <SettingPageClient exploitations={userExploitations || []} />;
+};
+
+export default SettingPage;
 // -------------------------------------------------------------
 
+// Server Component (Not working on fetching data on production)
+// -------------------------------------------------------------
 // import React, { Suspense } from "react";
 // import { OptionType } from "../components/selects/SingleSelect";
 // import Loading from "../components/Loading";
@@ -53,75 +119,4 @@
 //     return error;
 //   }
 // };
-
-// ------------------------------------------------------------------------
-
-// Client Component work on production :
-"use client";
-
-import React, { useEffect, useState } from "react";
-import { OptionType } from "../components/selects/SingleSelect";
-import Loading from "../components/Loading";
-import SettingPageClient from "../components/clients/settings/SettingPageClient";
-import { Exploitation } from "../models/interfaces/Exploitation";
-import { useSession } from "next-auth/react";
-
-const SettingPage = () => {
-  const { data: session, status } = useSession(); // Use useSession hook to access session data
-  console.log("session :", session);
-
-  const [userExploitations, setUserExploitations] = useState<
-    OptionType[] | null
-  >(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchExploitations = async () => {
-      if (!session?.user?.name) {
-        setLoading(false);
-        return;
-      }
-
-      try {
-        // const response = await fetch(
-        //   `${
-        //     process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000"
-        //   }/api/exploitations?userUID=${session.user.name}`
-        // );
-
-        const response = await fetch(
-          `http://127.0.0.1:3000/api/exploitations?userUID=${session.user.name}`
-        );
-
-        console.log("response :", response);
-
-        if (response.ok) {
-          const exploitations: Exploitation[] = await response.json();
-          const exploitationOptions = exploitations.map(exploitation => ({
-            id: exploitation.id,
-            value: exploitation.nom,
-            label: exploitation.nom,
-          }));
-
-          setUserExploitations(exploitationOptions);
-        } else {
-          console.error("Failed to fetch exploitations: ", response.status);
-        }
-      } catch (error) {
-        console.error("Error fetching exploitations: ", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchExploitations();
-  }, [session]);
-
-  if (status === "loading" || loading) {
-    return <Loading />;
-  }
-
-  return <SettingPageClient exploitations={userExploitations || []} />;
-};
-
-export default SettingPage;
+// -------------------------------------------------------------
